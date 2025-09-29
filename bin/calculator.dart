d0c077291f5ba366312c4963fd07e93a7ca5fbe2
AbdillahFajar <fajarabdillah2264@gmail.com>
2025-09-29 07:39:36 +0700

    calculator ver 2.0

--- bin/calculator.dart
+++ bin/calculator.dart
@@ -11,53 +11,49 @@ double _readNumber(String label) {
   }
 }
 
+void menu() {
+  print('\n=== Kalkulator Sederhana ===');
+  print('1) Tambah');
+  print('2) Kurang');
+  print('3) Kali');
+  print('4) Bagi');
+  print('5) Keluar');
+  stdout.write('Pilih [1-5]: ');
+  final pilih = stdin.readLineSync()?.trim();
+  if (pilih == '5') {
+    print('Bye!');
+    return;
+  } else if (pilih == '1') {
+    print('\nAnda memilih operasi penjumlahan');
+  } else if (pilih == '2') {
+    print('\nAnda memilih operasi pengurangan');
+  } else if (pilih == '3') {
+    print('\nAnda memilih operasi perkalian');
+  } else if (pilih == '4') {
+    print('\nAnda memilih operasi pembagian');
+  }
+  final a = _readNumber('Masukkan angka pertama');
+  final b = _readNumber('Masukkan angka kedua');
+  switch (pilih) {
+    case '1':
+      print('Hasil: ${a + b}');
+      break;
+    case '2':
+      print('Hasil: ${a - b}');
+      break;
+    case '3':
+      print('Hasil: ${a * b}');
+      break;
+    case '4':
+      print('Hasil: ${a / b}');
+      break;
+    default:
+      print('Pilihan tidak dikenal.');
+  }
+}
+
 void main() {
   while (true) {
-    print('\n=== Kalkulator Sederhana ===');
-    print('1) Tambah');
-    print('2) Kurang');
-    print('3) Kali');
-    print('4) Bagi');
-    print('5) Keluar');
-    stdout.write('Pilih [1-5]: ');
-    final pilih = stdin.readLineSync()?.trim();
-    if (pilih == '5') {
-      print('Bye!');
-      return;
-    }
-    else if (pilih == '1')
-    {
-      print('\nAnda memilih operasi penjumlahan');
-    }
-    else if (pilih == '2')
-    {
-      print('\nAnda memilih operasi pengurangan');
-    }
-    else if (pilih == '3')
-    {
-      print('\nAnda memilih operasi perkalian');
-    }
-    else if (pilih == '4')
-    {
-      print('\nAnda memilih operasi pembagian');
-    }
-    final a = _readNumber('Masukkan angka pertama');
-    final b = _readNumber('Masukkan angka kedua');
-    switch (pilih) {
-      case '1':
-        print('Hasil: ${a + b}');
-        break;
-      case '2': 
-        print('Hasil: ${a - b}');
-        break;
-      case '3':
-        print('Hasil: ${a * b}');
-        break;
-      case '4':
-        print('Hasil: ${a / b}'); 
-        break;
-      default:
-        print('Pilihan tidak dikenal.');
-    }
+    menu();
   }
 }
