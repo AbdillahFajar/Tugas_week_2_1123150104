4d536beb070cbfb837b57490f013041e1f593ef1
AbdillahFajar <fajarabdillah2264@gmail.com>
2025-09-29 20:14:19 +0700

    kalkulator 2.5

--- bin/calculator.dart
+++ bin/calculator.dart
@@ -24,6 +24,47 @@ int pembagianBulat(double a, double b) => a ~/ b;
 
 double sisaBagi(double a, double b) => a % b;
 
+void choosingOption(pilih) {
+  if (pilih == '1') {
+    print('\nAnda memilih operasi Penjumlahan');
+  } else if (pilih == '2') {
+    print('\nAnda memilih operasi Pengurangan');
+  } else if (pilih == '3') {
+    print('\nAnda memilih operasi Perkalian');
+  } else if (pilih == '4') {
+    print('\nAnda memilih operasi Pembagian');
+  } else if (pilih == '5') {
+    print('\nAnda memilih operasi Pembagian Bulat');
+  } else if (pilih == '6') {
+    print('\nAnda memilih operasi Modulus');
+  }
+}
+
+void printResult(pilih) {
+  final a = _readNumber('Masukkan angka pertama');
+  final b = _readNumber('Masukkan angka kedua');
+  switch (pilih) {
+    case '1':
+      print('Hasil =  ${a + b}');
+      break;
+    case '2':
+      print('Hasil =  ${a - b}');
+      break;
+    case '3':
+      print('Hasil =  ${a * b}');
+      break;
+    case '4':
+      print('Hasil =  ${a / b}');
+      break;
+    case '5':
+      print('Hasil =  ${a ~/ b}');
+      break;
+    case '6':
+      print('Hasil =  ${a % b}');
+      break;
+  }
+}
+
 void menu() {
   while (true) {
     //Awal loop
@@ -35,83 +76,21 @@ void menu() {
     print('5) Pembagian Bulat');
     print('6) Modulus');
     print('7) Keluar');
-    stdout.write('Pilih [1-7]: ');
+    stdout.write('Pilih [1-7]: '); //Ini mengisi variabel $label yang dibuat di _readNumber
     final pilih = stdin.readLineSync()?.trim();
+    choosingOption(pilih);
+    /* Membuat aturan logika untuk membatasi pilihan yang dipilih:
+       Kalau pilihannya selain dari pilihan 1–6, langsung ke default.*/
     if (pilih == '7') {
+      //Start Choosing Option
       print('Bye!');
       return;
-    } else if (pilih == '1') {
-      print('\nAnda memilih operasi Penjumlahan');
-    } else if (pilih == '2') {
-      print('\nAnda memilih operasi Pengurangan');
-    } else if (pilih == '3') {
-      print('\nAnda memilih operasi Perkalian');
-    } else if (pilih == '4') {
-      print('\nAnda memilih operasi Pembagian');
-    } else if (pilih == '5') {
-      print('\nAnda memilih operasi Pembagian Bulat');
-    } else if (pilih == '6') {
-      print('\nAnda memilih operasi Modulus');
-    }else{
-      print('Menu yang Anda pilih, tidak ada. Silahkan pilih menu yang sesuai.');
-      continue;
-      //Ini untuk pengganti default di switch case-nya. Karena udah saya hapus.
     }
-
-    /* Membuat aturan logika untuk membatasi pilihan yang dipilih
-    // Kalau pilihannya selain dari pilihan 1–6, langsung ke default
-    if (!(["1","2","3","4","5","6"].contains(pilih))) {
+    else if (!(["1", "2", "3", "4", "5", "6"].contains(pilih))) {
       print('Menu yang Anda pilih tidak ada. Silahkan pilih menu yang sesuai.');
       continue; // Kembali ke awal loop
     }
-
-    // Tampilkan info pilihan
-    switch (pilih) {
-      case '1':
-        print('\nAnda memilih operasi Penjumlahan');
-        break;
-      case '2':
-        print('\nAnda memilih operasi Pengurangan');
-        break;
-      case '3':
-        print('\nAnda memilih operasi Perkalian');
-        break;
-      case '4':
-        print('\nAnda memilih operasi Pembagian');
-        break;
-      case '5':
-        print('\nAnda memilih operasi Pembagian Bulat');
-        break;
-      case '6':
-        print('\nAnda memilih operasi Modulus');
-        break;
-    }*/
-    
-    final a = _readNumber('Masukkan angka pertama');
-    final b = _readNumber('Masukkan angka kedua');
-
-    switch (pilih) {
-      case '1':
-        print('Hasil =  ${a + b}');
-        break;
-      case '2':
-       print('Hasil =  ${a - b}');
-        break;
-      case '3':
-        print('Hasil =  ${a * b}');
-        break;
-      case '4':
-        print('Hasil =  ${a / b}');
-        break;
-      case '5':
-        print('Hasil =  ${a ~/ b}');
-        break;
-      case '6':
-        print('Hasil =  ${a % b}');
-        break;
-      // default:
-      // print("Menu yang Anda pilih, tidak ada. Silahkan pilih menu yang sesuai.");
-    }
+    printResult(pilih);
   }
 }
 
