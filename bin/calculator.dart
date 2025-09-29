203889546218f9daa2f64a707c3999dd025f5d35
AbdillahFajar <fajarabdillah2264@gmail.com>
2025-09-29 14:22:29 +0700

    calculator 2.2 is back

--- bin/calculator.dart
+++ bin/calculator.dart
@@ -11,29 +11,29 @@ double _readNumber(String label) {
   }
 }
 
-//Buat rumus matematika pake fungsi dengan nilai return
-double penjumlahan(double a, double b) {
-  return a + b;
+//Buat rumus matematika pake fungsi dengan parameter
+void penjumlahan(double a, double b) {
+  print('Hasil = ${a + b}');
 }
 
-double pengurangan(double a, double b) {
-  return a - b;
+void pengurangan(double a, double b) {
+  print('Hasil = ${a - b}');
 }
 
-double perkalian(double a, double b) {
-  return a * b;
+void perkalian(double a, double b) {
+  print('Hasil = ${a * b}');
 }
 
-double pembagian(double a, double b) {
-  return a / b;
+void pembagian(double a, double b) {
+  print('Hasil = ${a / b}');
 }
 
-int pembagianBulat(double a, double b) {
-  return a ~/ b;
+void pembagianBulat(double a, double b) {
+  print('Hasil = ${a ~/ b}');
 }
 
-double sisaBagi(double a, double b) {
-  return a % b;
+void sisaBagi(double a, double b) {
+  print('Hasil = ${a % b}');
 }
 
 void menu() {
@@ -73,22 +73,22 @@ void menu() {
 
     switch (pilih) {
       case '1':
-        print('Hasil: ${a + b}');
+        penjumlahan(a, b);
         break;
       case '2':
-       print('Hasil: ${a - b}');
+        pengurangan(a, b);
         break;
       case '3':
-        print('Hasil: ${a * b}');
+        perkalian(a, b);
         break;
       case '4':
-        print('Hasil: ${a / b}');
+        pembagian(a, b);
         break;
       case '5':
-        print('Hasil: ${a ~/ b}');
+        pembagianBulat(a, b);
         break;
       case '6':
-        print('Hasil: ${a % b}');
+        sisaBagi(a, b);
         break;
       default:
         print('Menu yang Anda pilih, tidak ada. Silahkan pilih menu yang sesuai.');
