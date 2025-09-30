0b0fa03de380de13c0f63fe3070cc64f5a9f8a6a
AbdillahFajar <fajarabdillah2264@gmail.com>
2025-09-30 13:51:21 +0700

    calculator 2.6

--- bin/calculator.dart
+++ bin/calculator.dart
@@ -24,43 +24,52 @@ int pembagianBulat(double a, double b) => a ~/ b;
 
 double sisaBagi(double a, double b) => a % b;
 
-void choosingOption(pilih) {
-  if (pilih == '1') {
-    print('\nAnda memilih operasi Penjumlahan');
-  } else if (pilih == '2') {
-    print('\nAnda memilih operasi Pengurangan');
-  } else if (pilih == '3') {
-    print('\nAnda memilih operasi Perkalian');
-  } else if (pilih == '4') {
-    print('\nAnda memilih operasi Pembagian');
-  } else if (pilih == '5') {
-    print('\nAnda memilih operasi Pembagian Bulat');
-  } else if (pilih == '6') {
-    print('\nAnda memilih operasi Modulus');
+//Mengubah isi pada fungsi choosingoption, dari logika if-else ke switch case return
+String choosingOption(pilih) {
+  switch(pilih)
+  {
+    case '1':
+      return "\nAnda memilih operasi Penjumlahan";
+    case '2':
+      return "\nAnda memilih operasi Pengurangan";
+    case '3':
+      return "\nAnda memilih operasi Perkalian";
+    case '4':
+      return "\nAnda memilih operasi Pembagian";
+    case '5':
+      return "\nAnda memilih operasi Pembagian Bulat";
+    case '6':
+      return "\nAnda memilih operasi Modulus";
+    case '7':
+      print("Bye!");
+      exit(0); //Mematikan program
+    default:
+      return "Menu yang Anda pilih, tidak tersedia. Silahkan pilih menu yang sesuai.";
   }
 }
 
+//Fungsi printResult untuk menampilkan hasil dari angka yang dimasukkan user. Fungsi ini menggunakan fungsi void yang tidak mengembalikan nilai (return)
 void printResult(pilih) {
   final a = _readNumber('Masukkan angka pertama');
   final b = _readNumber('Masukkan angka kedua');
   switch (pilih) {
     case '1':
-      print('Hasil =  ${a + b}');
+      print('Hasil = ${penjumlahan (a, b)}');
       break;
     case '2':
-      print('Hasil =  ${a - b}');
+      print('Hasil = ${pengurangan (a, b)}');
       break;
     case '3':
-      print('Hasil =  ${a * b}');
+      print('Hasil = ${perkalian (a, b)}');
       break;
     case '4':
-      print('Hasil =  ${a / b}');
+      print('Hasil = ${pembagian (a, b)}');
       break;
     case '5':
-      print('Hasil =  ${a ~/ b}');
+      print('Hasil = ${pembagianBulat (a, b)}');
       break;
     case '6':
-      print('Hasil =  ${a % b}');
+      print('Hasil = ${sisaBagi (a, b)}');
       break;
   }
 }
@@ -78,15 +87,10 @@ void menu() {
     print('7) Keluar');
     stdout.write('Pilih [1-7]: '); //Ini mengisi variabel $label yang dibuat di _readNumber
     final pilih = stdin.readLineSync()?.trim();
-    choosingOption(pilih);
+    print(choosingOption (pilih));
     /* Membuat aturan logika untuk membatasi pilihan yang dipilih:
-       Kalau pilihannya selain dari pilihan 1–6, langsung ke default.*/
-    if (pilih == '7') {
-      print('Bye!');
-      return;
-    }
-    else if (!(["1", "2", "3", "4", "5", "6"].contains(pilih))) {
-      print('Menu yang Anda pilih tidak ada. Silahkan pilih menu yang sesuai.');
+       Kalau pilihannya selain dari pilihan 1–6, langsung balik ke awal loop.*/
+    if (!(["1", "2", "3", "4", "5", "6"].contains(pilih))) {
       continue; // Kembali ke awal loop
     }
     printResult(pilih);
